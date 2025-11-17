7f6a53bd12de3edf723bd24548eeff5b44bf72fe
msouza-dev <msouza@email.rs>
2025-11-17 12:06:33 -0300

    adicionado botao que leva para a tabela e adicionado funções em script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botao = document.getElementById("btnTabela");
 
-
+    botao.addEventListener("click", function() {
+        window.location.href = "https://ge.globo.com/futebol/brasileirao-serie-a/";
+    });
+});
